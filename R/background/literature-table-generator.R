7d75352730d29501a606601077fae7d30602e76e
David.Ellis <david.ellis@birmingham.gov.uk>
2026-03-06 19:29:06 +0000

    Code for vaccine hesitancy papers

--- R/background/literature-table-generator.R
+++ R/background/literature-table-generator.R
@@ -1,5 +1,9 @@
 library(readxl)
 
+################################################################################
+#                            Measles Modelling                                 #
+################################################################################
+
 modelling_refs <- read_excel(
   "data/background/LiteratureSearch.xlsx",
   sheet = "Modelling"
@@ -15,6 +19,29 @@ for (i in 1:nrow(modelling_refs)) {
     "\\citetitle{%s} & \\citeauthor*{%s} & \\citeyear{%s} & %s & \\cite{%s} \\\\ %s",
     latex_ids[i], latex_ids[i], latex_ids[i], sim_types[i], latex_ids[i],
     ifelse(i == nrow(modelling_refs), "", "\\midrule") 
+  ))
+  cat("\n")
+}
+
+################################################################################
+#                         Measles Vaccine Hesitancy                            #
+################################################################################
+
+
+hesitancy_refs <- read_excel(
+  "data/background/LiteratureSearch.xlsx",
+  sheet = "Hesitancy"
+)
+
+h_latex_ids <- hesitancy_refs$LatexID
+
+cat("\n\n\n\n")
+
+for (i in 1:nrow(hesitancy_refs)) {
+  cat(sprintf(
+    "\\citetitle{%s} & \\citeauthor*{%s} & \\citeyear{%s} & \\cite{%s} \\\\ %s",
+    h_latex_ids[i], h_latex_ids[i], h_latex_ids[i], h_latex_ids[i],
+    ifelse(i == nrow(hesitancy_refs), "", "\\midrule") 
   ))
   cat("\n")
 }
