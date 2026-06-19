5fb2e8728c40bf9617391759d25d6a9182ddbaae
David.Ellis <david.ellis@birmingham.gov.uk>
2026-06-19 14:02:06 +0100

    Tuned full model and plotted infection curve

--- R/model params/full-model-tuning.R
+++ R/model params/full-model-tuning.R
@@ -1,127 +1,14 @@
+# Tune full model
+
 library(readxl)
 library(ggplot2)
 library(dplyr)
 library(tidyr)
 library(latex2exp)
 
-################################################################################
-#                     Varying isolation proportion                             #
-################################################################################
-
-isol_props <- read_excel(
-  "data/stella outputs/basic-model-testing/SIR-isolation-params-single.xlsx",
-  sheet = "isol-prop-runs"
-) %>%
-  pivot_longer(
-    cols = contains("Run"),
-    names_to = "Run",
-    values_to = "Infected"
-  ) %>%
-  left_join(
-    read_excel(
-      "data/stella outputs/basic-model-testing/SIR-isolation-params-single.xlsx",
-      sheet = "isol-prop-params"
-    ),
-    by = join_by("Run")
-  ) %>%
-  filter(
-    Run != "Run 1"
-  ) %>%
-  mutate(
-    group = factor(`isolation proportion`)
-  )
-
-ggplot(isol_props, aes(x = Day, y = Infected, color = `isolation proportion`,
-                       group = group)) +
-  geom_line() +
-  theme_bw() +
-  scale_x_continuous(
-    limits = c(0, 150),
-    expand = c(0,0)
-  ) +
-  scale_y_continuous(
-    limits = c(0, 30),
-    expand = c(0,0)
-  ) +
-  labs(
-    x = "Days Since Outbreak Start",
-    y = TeX("Number of People Infected, $I(t)$"),
-    color = "Isolation Proportion"
-  ) +
-  theme(
-    #legend.direction = "horizontal",
-    legend.position = "top",
-    legend.margin = margin_auto(0.5),
-    margins = margin_auto(10)
-  )
-
-ggsave("figures/model-params/SIR-isol-prop.pdf", 
-       width = 6, height = 3)
-
-
-################################################################################
-#                     Varying isolation threshold                              #
-################################################################################
-
-isol_thresh <- read_excel(
-  "data/stella outputs/basic-model-testing/SIR-isolation-params-single.xlsx",
-  sheet = "isol-thresh-runs"
-) %>%
-  pivot_longer(
-    cols = contains("Run"),
-    names_to = "Run",
-    values_to = "Infected"
-  ) %>%
-  left_join(
-    read_excel(
-      "data/stella outputs/basic-model-testing/SIR-isolation-params-single.xlsx",
-      sheet = "isol-thresh-params"
-    ),
-    by = join_by("Run")
-  ) %>%
-  filter(
-    Run != "Run 1"
-  ) %>%
-  mutate(
-    group = factor(`isolation threshold`)
-  )
-
-ggplot(isol_thresh, aes(x = Day, y = Infected, color = `isolation threshold`,
-                        group = group)) +
-  geom_line() +
-  theme_bw() +
-  scale_x_continuous(
-    limits = c(0, 150),
-    expand = c(0,0)
-  ) +
-  scale_y_continuous(
-    limits = c(0, 30),
-    expand = c(0,0)
-  ) +
-  labs(
-    x = "Days Since Outbreak Start",
-    y = TeX("Number of People Infected, $I(t)$"),
-    color = "Isolation Threshold"
-  ) +
-  theme(
-    #legend.direction = "horizontal",
-    legend.position = "top",
-    legend.margin = margin_auto(0.5),
-    margins = margin_auto(10)
-  )
-
-ggsave("figures/model-params/SIR-isol-thresh.pdf", 
-       width = 6, height = 3)
-
-
-################################################################################
-#                              Varying both                                    #
-################################################################################
-
-
 isol_params_both <- read_excel(
-  "data/stella outputs/basic-model-testing/SIR-isolation-params-both.xlsx",
-  sheet = "run-data2"
+  "data/stella outputs/basic-model-testing/full-isolation-params.xlsx",
+  sheet = "run-data"
 ) %>%
   pivot_longer(
     cols = contains("Run"),
@@ -130,20 +17,21 @@ isol_params_both <- read_excel(
   ) %>%
   left_join(
     read_excel(
-      "data/stella outputs/basic-model-testing/SIR-isolation-params-both.xlsx",
-      sheet = "run-params2"
+      "data/stella outputs/basic-model-testing/full-isolation-params.xlsx",
+      sheet = "run-params"
     ),
     by = join_by("Run")
   ) 
 
 summary_stats <- isol_params_both %>%
-  group_by(`isolation threshold`, `isolation proportion`) %>%
+  group_by(`Isolation threshold`, `Isolation proportion`) %>%
   filter(
     Infections >= 0.5,
-    `isolation threshold` > 5,
-    `isolation proportion` < 0.15) %>%
+    `Isolation threshold` > 5,
+    `Isolation proportion` > 0.07,
+    `Isolation proportion` < 0.15) %>%
   summarise(
-    `Total Cases` = sum(Infections),
+    `Total Cases` = sum(Infections)/1000,
     `Outbreak Length` = max(Day),
     .groups = "drop"
   ) %>%
@@ -155,12 +43,12 @@ summary_stats <- isol_params_both %>%
 
 cases_plt <- summary_stats %>%
   filter(Indicator == "Total Cases") %>%
-  ggplot(aes(x = `isolation threshold`, 
-             y = `isolation proportion`,
+  ggplot(aes(x = `Isolation threshold`, 
+             y = `Isolation proportion`,
              fill= Value)) + 
   geom_raster(interpolate = TRUE) +
   geom_contour(aes(z = Value),
-               breaks = 422, 
+               breaks = 0.422, 
                color = "white",
                lwd = 1.2) +
   theme_bw() +
@@ -168,16 +56,17 @@ cases_plt <- summary_stats %>%
     expand = c(0,0)
   ) +
   scale_y_continuous(
-    expand = c(0,0)
+    expand = c(0,0),
+
   ) +
   scale_fill_continuous(
-    palette = viridis::viridis(30)
-  )+
+    palette = viridis::viridis(30),
+  ) +
   facet_wrap(
     ~ Indicator
   ) +
   labs(
-    fill = "Total Cases\n",
+    fill = "Total Cases\n(Thousands)",
     y = "Isolation Proportion",
     x = "Isolation Threshold"
   ) +
@@ -187,19 +76,24 @@ cases_plt <- summary_stats %>%
     strip.background = element_rect(fill="white")
   ) +
   annotate(
-    "text", x = 37, y = 0.068, label = "422 cases",
+    "text", x = 37, y = 0.121, label = "422 cases",
     color = "white",
-    angle = 33,
+    angle = 40,
     size = 4.5
-    )
+  )
 
 
 length_plt <- summary_stats %>%
   filter(Indicator == "Outbreak Length") %>%
-  ggplot(aes(x = `isolation threshold`, 
-             y = `isolation proportion`,
+  ggplot(aes(x = `Isolation threshold`, 
+             y = `Isolation proportion`,
              fill= Value)) + 
   geom_raster(interpolate = TRUE) +
+  scale_fill_continuous(
+    palette = viridis::magma(30),
+    limits = c(56, 522),
+    breaks = c(100,300,500)
+  ) +
   geom_contour(aes(z = Value),
                breaks = c(175), 
                color = "white",
@@ -210,10 +104,6 @@ length_plt <- summary_stats %>%
   ) +
   scale_y_continuous(
     expand = c(0,0)
-  ) + 
-  scale_fill_continuous(
-    palette = viridis::magma(30),
-    breaks = seq(150, 350, 100)
   ) +
   facet_wrap(
     ~ Indicator
@@ -228,9 +118,9 @@ length_plt <- summary_stats %>%
     strip.background = element_rect(fill="white")
   ) +
   annotate(
-    "text", x = 45, y = 0.08, label = "175 days",
+    "text", x = 45, y = 0.12, label = "175 days",
     color = "white",
-    angle = 22,
+    angle = 12,
     size = 4.5
   )
 
@@ -254,8 +144,8 @@ get_contour <- function(
     )
   
   interp_out <- akima::interp(
-    x = data_i$`isolation threshold`,
-    y = data_i$`isolation proportion`,
+    x = data_i$`Isolation threshold`,
+    y = data_i$`Isolation proportion`,
     z = data_i$Value
   )
   
@@ -279,9 +169,10 @@ get_contour <- function(
   
   return(contour_df)
 }
-c1 <- get_contour(summary_stats, "Total Cases", 422)
+c1 <- get_contour(summary_stats, "Total Cases", 0.422)
 c2 <- get_contour(summary_stats, "Outbreak Length", 175)
 contours <- rbind(c1, c2)
+
 
 # Find root
 f1 <- approxfun(c1$x, c1$y)
@@ -289,14 +180,18 @@ f2 <- approxfun(c2$x, c2$y)
 
 h <- function(x) f1(x) - f2(x)
 
-xroot <- uniroot(h, interval = range(c1$x))$root
+xroot <- uniroot(h, interval = c(
+  max(min(c1$x), min(c2$x)), 
+  min(max(c1$x), max(c2$x))
+  )
+  )$root
 yroot <- f1(xroot)
 
 cont_plt <- ggplot(contours, aes(x=x, y=y, color = Indicator)) +
   geom_line(lwd = 1.2) +
   geom_point(x = xroot, y = yroot, 
              color = "black", size = 3) +
-  annotate("text", x = xroot-6, y = yroot+0.008, 
+  annotate("text", x = xroot-4.5, y = yroot+0.004, 
            label = paste0("(", round(xroot, 1), ", ", round(yroot, 2),")"),
            size = 4.5) +
   theme_bw() +
@@ -323,11 +218,11 @@ cmb_plts <- cowplot::plot_grid(
   map_plts, cont_plt,
   ncol = 1,
   rel_heights = c(0.55, 0.45)
-  )
+)
 cmb_plts
 
 ggsave(
-  "figures/model-params/SIR-isol-params.pdf",
+  "figures/model-params/full-model-isol-params.pdf",
   plot = cmb_plts,
   width = 7.5, height = 7
 )
@@ -336,9 +231,38 @@ ggsave(
 #                               Tuned Model                                    #
 ################################################################################
 
-tuned_data <- read_excel(
-  "data/stella outputs/basic-model-testing/tuned-SIR-output.xlsx"
+tuned_data_full <- read_excel(
+  "data/stella outputs/basic-model-testing/tuned-full-output.xlsx"
 ) %>%
+  mutate(
+    `Total Infections` = `Baby infections`+`Child infections`+`Adult infections`
+  ) %>%
+  select(Day, `Total Infections`) %>%
+  mutate(
+    week_number = floor(Day / 7)+5,
+    testing_curve = `Total Infections`
+  ) %>%
+  pivot_longer(
+    cols = c("testing_curve"),
+    names_to = "indicator",
+    values_to = "value"
+  ) %>%
+  group_by(week_number, indicator) %>%
+  summarise(
+    value = sum(value)
+  ) %>%
+  mutate(
+    indicator = case_when(
+      indicator == "daily_infections" ~ "Weekly Infections (Full)",
+      indicator == "testing_curve" ~ "Full Model",
+      TRUE ~ "Error"
+    )
+  )
+
+
+tuned_data_SIR <- read_excel(
+  "data/stella outputs/basic-model-testing/tuned-SIR-output.xlsx"
+)  %>%
   mutate(
     week_number = floor(Day / 7),
     testing_curve = Infections
@@ -354,10 +278,17 @@ tuned_data <- read_excel(
   ) %>%
   mutate(
     indicator = case_when(
-      indicator == "testing_curve" ~ "Modelled Weekly Cases",
+      indicator == "daily_infections" ~ "Weekly Infections (SIR)",
+      indicator == "testing_curve" ~ "SIR + Isolation",
       TRUE ~ "Error"
     )
   )
+
+# Combine simple and full model outputs
+model_data <- rbind(
+  tuned_data_full,
+  tuned_data_SIR
+)
 
 cases <- read_excel(
   "data/model params/BSol-outbreak-cases-oct23toapril24.xlsx",
@@ -385,13 +316,14 @@ cases <- read_excel(
 ggplot(cases, aes(x = week_number, y = value)) +
   geom_col() +
   geom_line(
-    data = tuned_data %>% filter(indicator == "Modelled Weekly Cases"), 
+    data = model_data %>% 
+      filter(indicator %in% c("Full Model", "SIR + Isolation")), 
     aes(x = week_number, y = value, color = indicator),
     lwd = 1.05
-       ) +
+  ) +
   theme_bw() +
   scale_x_continuous(
-    limits = c(0, 30),
+    limits = c(0, 27),
     expand = c(0, 0)
   ) +
   scale_y_continuous(
@@ -399,15 +331,26 @@ ggplot(cases, aes(x = week_number, y = value)) +
     expand = c(0, 0)
   ) +
   labs(
-    y = "Measles Cases",
+    y = "Lab Confirmed Measles Cases",
     x = "Week Number",
     color = ""
   ) +
   theme(
     legend.position = "inside",
-    legend.position.inside = c(0.2, 0.97),
+    legend.position.inside = c(0.15, 0.90),
     legend.background = element_rect(fill=NA)
   )
 
-ggsave("figures/model-params/SIR-tuned.pdf", 
+ggsave("figures/model-params/full-tuned.pdf", 
        width = 6, height = 3)
+
+
+read_excel(
+  "data/stella outputs/basic-model-testing/tuned-full-output.xlsx",
+  sheet = 1
+) %>%
+  summarise(
+    `Baby infections` = sum(`Baby infections`),
+    `Child infections` = sum(`Child infections`),
+    `Adult infections` = sum(`Adult infections`)
+  )
