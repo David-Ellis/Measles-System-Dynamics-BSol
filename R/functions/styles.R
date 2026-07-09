15b69200a9b43ea2cf03c3db2f1285f4922f9435
David.Ellis <david.ellis@birmingham.gov.uk>
2026-07-09 10:57:41 +0100

    Plotted workshop flow factors

---
+++ R/functions/styles.R
@@ -0,0 +1,11 @@
+
+uob_colors <- c(
+  "#E30513", # Red
+  "#5E4F9C", # Purple
+  "#2581C4", # Blue
+  "#F07F3C", # Orange
+  "#3AAA35", # Green
+  "#EA5284", # Pink
+  "#00ACA9", # Turquoise
+  "#58595B" # Grey
+  )
---
+++ R/workshop/visualise-workshop-results.R
@@ -0,0 +1,61 @@
+library(readxl)
+library(dplyr)
+library(ggplot2)
+library(janitor)
+
+source("R/functions/styles.R")
+
+# Visualise most important flow factors
+
+flow_factors <- read_excel(
+  "data/workshop/flow-factors.xlsx"
+) %>%
+  clean_names() %>%
+  arrange(votes)
+
+flow_factors$factor = factor(
+  flow_factors$factor,
+  levels = flow_factors$factor)
+
+flow_plt <- ggplot(
+  flow_factors,
+  aes(
+    y = factor,
+    x = votes,
+    fill = theme,
+    label = votes
+  )) +
+  geom_col() +
+  geom_text(nudge_x = 0.3) +
+  scale_x_continuous(
+    expand = c(0,0),
+    limits = c(0, 8)
+  ) +
+  theme_bw() +
+  facet_grid(
+    theme~.,
+    #ncol=1,
+    scales = "free_y",
+    space = "free_y",
+    switch = "y"
+  )+
+  labs(
+    x = "Number of Votes",
+    y = ""
+  ) + 
+  scale_fill_manual(
+    values = uob_colors
+  )+
+  theme(
+    #strip.background = element_rect(fill="white"),
+    legend.position = "none",
+    strip.placement = "outside",
+    strip.text.y.left = element_text(face = "bold"),
+    strip.background = element_blank()
+  ) 
+flow_plt
+ggsave(
+  "figures/workshop/flow-factors.pdf", 
+  plot = flow_plt,
+  width = 5, height = 6
+  )
